22e2a3602329f88f0905b00519925918ae786ab1
raphael Abracos <raphael_batista_10@hotmail.com>
2021-05-13 17:31:15 -0300

    first commit

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+  </S.Wrapper>
+)
+
+export default Main
